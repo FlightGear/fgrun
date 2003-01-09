4aea8434bff0c14702b437d230b5906508f87b1e
Bernie Bright <bbright@users.sourceforge.net>
2003-01-09 12:00:38 +0000

    Added copyright notice. Added snprintf macro for MSVC6.
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@52 65451dfc-fe10-0410-907f-9363c9a62196

--- src/io.cxx
+++ src/io.cxx
@@ -1,4 +1,30 @@
+// io_list.cxx -- I/O option browser functions.
+//
+// Written by Bernie Bright, started Dec 2002.
+//
+// Copyright (c) 2002  Bernie Bright - bbright@bigpond.net.au
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// $Id$
+
 #include "UserInterface.h"
+
+#if defined(_MSC_VER)
+#define snprintf _snprintf
+#endif
 
 void
 UserInterface::io_list_select_cb( Fl_Browser* o )
