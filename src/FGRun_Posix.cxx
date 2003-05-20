45ec2d72146c87c1bb5462d6f0d8217baf7f7dd9
Bernie Bright <bbright@users.sourceforge.net>
2003-05-20 00:51:19 +0000

    Create a pseudo-tty and output window in case we are started from a destop icon.
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@67 65451dfc-fe10-0410-907f-9363c9a62196

--- src/FGRun_Posix.cxx
+++ src/FGRun_Posix.cxx
@@ -32,38 +32,76 @@
 #  include <sys/wait.h>
 #endif
 
+#ifdef HAVE_PTY_H
+#  include <pty.h>
+#endif
+
+#ifdef HAVE_TERMIOS_H
+#  include <termios.h>
+#endif
+
+#include <iostream>
 #include <string>
+#include <FL/Fl_Text_Display.H>
 
 #include "FGRun_Posix.h"
 
 using std::string;
+using std::cout;
 
 FGRun_Posix::FGRun_Posix()
+    : win(0)
+    , view(0)
 {
 }
 
 FGRun_Posix::~FGRun_Posix()
 {
+    delete win;
+}
+
+void
+FGRun_Posix::create_output_window()
+{
+    win = new Fl_Window( 640, 480, "FlightGear output" );
+    view = new Fl_Text_Display( 0, 0, 640, 480 );
+    view->buffer( new Fl_Text_Buffer );
 }
 
 void
 FGRun_Posix::run_fgfs_impl()
 {
-    pid_t pid = fork();
+    struct termios term;
+    tcgetattr( STDOUT_FILENO, &term );
+    term.c_oflag &= ~( OLCUC | ONLCR );
+
+    int master;
+    pid_t pid = forkpty( &master, 0, &term, 0 );
+
     if (pid < 0)
     {
 	perror( "fork error" );
+	close( master );
 	return;
     }
 
     if (pid > 0)
     {
 	// parent
+// 	cout << "pty master fd =" << master << "\n";
+
+	delete win;
+	create_output_window();
+	win->show();
+
+	Fl::add_fd( master, stdout_cb, this );
 	return;
     }
     else
     {
 	// child
+	close( master );
+
 	string path = fg_exe->value();
 	string arg0;
 	string::size_type idx = path.find_last_of( "/\\" );
@@ -79,4 +117,59 @@ FGRun_Posix::run_fgfs_impl()
 	execl( path.c_str(), arg0.c_str(), NULL );
     }
 }
+
+void
+FGRun_Posix::stdout_cb( int fd, void* p )
+{
+    static_cast<FGRun_Posix*>(p)->stdout_cb( fd );
+}
+
+void
+FGRun_Posix::stdout_cb( int fd )
+{
+    char buf[256];
+    ssize_t n = read( fd, buf, sizeof( buf ) - 1 );
+    if (n > 0)
+    {
+	buf[n] = 0;
+	// Ensure new text is added at the end.
+	view->insert_position( view->buffer()->length() );
+	view->insert( buf );
+	view->show_insert_position();
+    }
+    else
+    {
+	Fl::remove_fd( fd );
+	close( fd );
+
+	int status;
+	int pid = wait( &status );
+    }
+}
+
+// void
+// FGRun_Posix::stderr_cb( int fd, void* p )
+// {
+//     static_cast<FGRun_Posix*>(p)->stderr_cb( fd );
+// }
+
+// void
+// FGRun_Posix::stderr_cb( int fd )
+// {
+//     char buf[256];
+//     ssize_t n = read( fd, buf, sizeof( buf ) - 1 );
+//     if (n > 0)
+//     {
+// 	printf( "stderr: %d\n", n );
+// 	buf[n] = 0;
+//     }
+//     else
+//     {
+// 	Fl::remove_fd( fd );
+// 	close( fd );
+
+// 	int status;
+// 	waitpid( -1, &status, WNOHANG );
+//     }
+// }
 
--- src/FGRun_Posix.h
+++ src/FGRun_Posix.h
@@ -3,6 +3,9 @@
 
 #include "UserInterface.h"
 
+class Fl_Window;
+class Fl_Text_Display;
+
 class FGRun_Posix : public UserInterface
 {
 public:
@@ -11,6 +14,18 @@ public:
 
 private:
     void run_fgfs_impl();
+
+    static void stdout_cb( int fd, void* arg );
+    void stdout_cb( int fd );
+
+    static void stderr_cb( int fd, void* arg );
+    void stderr_cb( int fd );
+
+    void create_output_window();
+
+private:
+    Fl_Window* win;
+    Fl_Text_Display* view;
 };
 
 #endif // FGRUN_POSIX_H
