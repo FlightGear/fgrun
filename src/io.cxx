c091b11f618cf7abd1c481009c5a353e01594cb2
Bernie Bright <bbright@users.sourceforge.net>
2003-10-11 00:11:15 +0000

    Added file field to --generic protocol. Eliminated potential buffer overflows when parsing protocol strings.
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@149 65451dfc-fe10-0410-907f-9363c9a62196

--- src/io.cxx
+++ src/io.cxx
@@ -20,10 +20,18 @@
 //
 // $Id$
 
+#include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
+#include <FL/Fl_File_Chooser.h>
+#include <FL/filename.h>
 
 #include "UserInterface.h"
 #include "util.h"
+
+using std::string;
+using std::vector;
 
 void
 UserInterface::io_list_select_cb( Fl_Browser* o )
@@ -37,53 +45,61 @@ UserInterface::io_list_select_cb( Fl_Browser* o )
 	io_dir->activate();
 	io_hz->activate();
 
-	const char* p = o->text(n);
-	char protocol[32];
-	char medium[32];
-	char dir[32];
-	int hz = 0;
-	char args[256];
+	// Split output string into tokens.
+	string buf = o->text(n);
+	vector< string > tokens;
+	const string delims = "=,";
+	string::size_type op = 0;
+	string::size_type np;
 
-	sscanf( p, "--%[^=]=%[^,],%[^,],%d,%s",
-		protocol, medium, dir, &hz, args );
-	set_choice( io_protocol, protocol );
-	set_choice( io_medium, medium );
+	while ((np = buf.find_first_of( delims, op )) != buf.npos)
+	{
+	    tokens.push_back( buf.substr( op, np-op ) );
+	    op = np + 1;
+	    // 	if ((op = buf.find_first_not_of( delims, np )) == buf.npos)
+	    // 	    break;
+	}
+	tokens.push_back( buf.substr( op, buf.npos ) );
+
+	// Skip leading "--" of protocol token.
+	set_choice( io_protocol, tokens[0].c_str() + 2 );
+	set_choice( io_medium, tokens[1].c_str() );
 	io_medium->do_callback();
-	set_choice( io_dir, dir );
-	io_hz->value( hz );
+	set_choice( io_dir, tokens[2].c_str() );
+	io_hz->value( strtod( tokens[3].c_str(), 0 ) );
 
-	if (strcmp(medium, "file") == 0)
+	if (tokens[1] == "file")
 	{
-	    file_name->value( args );
+	    io_file_name->value( tokens[4].c_str() );
+	    generic_group->hide();
 	}
-	else if (strcmp(medium, "serial") == 0)
+	else if (tokens[1] == "serial")
 	{
-	    char device[256];
-	    char baud[32];
-	    sscanf(args, "%[^,],%s", device, baud );
-	    serial_port->value( device );
-	    serial_baud_rate->value( baud );
+	    serial_port->value( tokens[4].c_str() );
+	    serial_baud_rate->value( tokens[5].c_str() );
+	    generic_group->hide();
 	}
-	else if (strcmp(medium, "socket") == 0)
+	else if (tokens[1] == "socket")
 	{
-	    char host[128];
-	    int port;
-	    char type[32];
-	    if (args[0] == ',')
-	    {
-		host[0] = 0;
-		sscanf(args, ",%d,%s", &port, type );
-	    }
-	    else
-	    {
-		sscanf(args, "%[^,],%d,%s", host, &port, type );
-	    }
-	    socket_host->value(host);
-	    socket_port->value( port );
-	    if (strcmp(type, "tcp") == 0)
+	    socket_host->value( tokens[4].c_str() );
+	    socket_port->value( atoi( tokens[5].c_str() ) );
+	    if (tokens[6] == "tcp")
 		socket_tcp->setonly();
 	    else
 		socket_udp->setonly();
+
+	    if (tokens[0] == "--generic")
+	    {
+		io_generic_file->value( tokens[7].c_str() );
+
+		generic_group->position( socket_group->x(),
+			     socket_group->y() + socket_group->h() );
+		generic_group->show();
+	    }
+	    else
+	    {
+		generic_group->hide();
+	    }
 	}
     }
 }
@@ -106,14 +122,16 @@ UserInterface::io_list_update_cb()
 {
     std::ostringstream oss;
 
-    // --protocol=medium,dir,hz,filename|(device,baud)|(host,port,tcp|udp)
+    // --PROTOCOL=file,DIR,HZ,FILENAME
+    // --PROTOCOL=serial,DIR,HZ,DEVICE,BAUD
+    // --PROTOCOL=socket,DIR,HZ,HOST,PORT,(tcp|udp)[,FILENAME]
 
     if (strcmp(io_medium->text(), "file") == 0)
     {
 	oss << "--" << io_protocol->text()
 	    << "=file," << io_dir->text()
 	    << "," << int(io_hz->value())
-	    << "," << file_name->value();
+	    << "," << io_file_name->value();
     }
     else if (strcmp(io_medium->text(), "serial") == 0)
     {
@@ -131,6 +149,13 @@ UserInterface::io_list_update_cb()
 	    << "," << socket_host->value()
 	    << "," <<  int(socket_port->value())
 	    << (socket_tcp->value() ? ",tcp" : ",udp");
+
+	if (strcmp( io_protocol->text(), "generic" ) == 0)
+	{
+	    oss << ",";
+	    if (io_generic_file->size() > 0)
+		oss << io_generic_file->value();
+	}
     }
 
     int n = io_list->value();
@@ -141,7 +166,7 @@ UserInterface::io_list_update_cb()
 }
 
 void
-UserInterface::io_list_medium_update_cb( Fl_Choice* o )
+UserInterface::io_medium_update_cb( Fl_Choice* o )
 {
     if (strcmp(o->text(), "file") == 0)
     {
@@ -149,6 +174,7 @@ UserInterface::io_list_medium_update_cb( Fl_Choice* o )
 	menu_io_dir[2].deactivate();
 	serial_group->hide();
 	socket_group->hide();
+        generic_group->hide();
     }
     else if (strcmp(o->text(), "serial") == 0)
     {
@@ -156,6 +182,7 @@ UserInterface::io_list_medium_update_cb( Fl_Choice* o )
 	serial_group->show();
 	socket_group->hide();
 	menu_io_dir[2].activate();
+        generic_group->hide();
     }
     else if (strcmp(o->text(), "socket") == 0)
     {
@@ -163,7 +190,76 @@ UserInterface::io_list_medium_update_cb( Fl_Choice* o )
 	serial_group->hide();
 	socket_group->show();
 	menu_io_dir[2].activate();
+
+	generic_group->position( socket_group->x(),
+				 socket_group->y() + socket_group->h() );
+	generic_group->show();
     }
 
     io_list_update_cb();
 }
+
+void
+UserInterface::io_protocol_update_cb()
+{
+    io_list_update_cb();
+
+    if (strcmp( io_protocol->text(), "generic" ) == 0 &&
+	strcmp( io_medium->text(), "socket" ) == 0)
+    {
+        Fl_Group* group;
+
+//         if (file_group->visible())
+//             group = file_group;
+//         else if (serial_group->visible())
+//             group = serial_group;
+//         else if (socket_group->visible())
+            group = socket_group;
+//         else
+//         {
+//             return;
+//         }
+
+	generic_group->position( socket_group->x(),
+				 socket_group->y() + socket_group->h() );
+	generic_group->show();
+    }
+    else
+    {
+        generic_group->hide();
+    }
+}
+
+void
+UserInterface::io_file_cb()
+{
+}
+
+void
+UserInterface::io_generic_file_cb()
+{
+    //static Fl_File_Chooser* fc = 0;
+    string dir = fg_root->value();
+    dir += "/Protocol";
+    const char* pat = "*.xml";
+    const char* message = "Select protocol file";
+
+    Fl_File_Chooser fc( dir.c_str(), pat, Fl_File_Chooser::SINGLE, message );
+    fc.show();
+
+    while ( fc.shown() )
+	Fl::wait();
+
+    if (fc.value())
+    {
+	const char* fname = fl_filename_name( fc.value() );
+	const char* ext = fl_filename_ext( fname );
+	if (ext != 0 && *ext != 0)
+	{
+	    string f( fname, ext );
+	    io_generic_file->value( f.c_str() );
+	    io_list_update_cb();
+	}
+    }
+}
+
