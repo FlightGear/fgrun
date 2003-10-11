4bc7135fecd2ed27164fbb77a0b9d62bac09fcec
Bernie Bright <bbright@users.sourceforge.net>
2003-10-11 00:16:08 +0000

    Moved some callbacks.
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@151 65451dfc-fe10-0410-907f-9363c9a62196

--- src/init.cxx
+++ src/init.cxx
@@ -1,4 +1,7 @@
+#include <sstream>
 #include <FL/filename.h>
+#include <FL/Fl_File_Chooser.h>
+
 #include "UserInterface.h"
 #include "airportdb.h"
 
@@ -35,4 +38,56 @@ UserInterface::init()
     }
 
     update_airports_cb();
+}
+
+void
+UserInterface::fg_exe_cb()
+{
+    char* p = fl_file_chooser("Select executable", 0, fg_exe->value(), 0);
+    if (p != 0)
+    {
+	fg_exe->value( p );
+	run->activate();
+    }
+}
+
+void
+UserInterface::fg_root_cb()
+{
+    char* p = fl_dir_chooser( "Select FG_ROOT directory",
+			      fg_root->value(), 0);
+    if (p != 0)
+    {
+	fg_root->value( p );
+	std::ostringstream oss;
+	oss << p << "/Scenery";
+
+	if (fl_filename_isdir( oss.str().c_str() ))
+	{
+	    fg_scenery->value( oss.str().c_str() );
+	    airport_update->activate();
+	    aircraft_update->activate();
+	}
+    }
+}
+
+void
+UserInterface::fg_scenery_cb()
+{
+    char* p = fl_dir_chooser("Select FG_SCENERY directory",
+			     fg_scenery->value(), 0);
+    if (p != 0)
+    {
+	fg_scenery->value( p );
+	airport_update->activate();
+	aircraft_update->activate();
+    }
+}
+
+void
+UserInterface::fg_browser_cb()
+{
+    char* p = fl_file_chooser("Select browser", 0, browser->value(), 0);
+    if (p != 0)
+	browser->value( p );
 }
