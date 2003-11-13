42afa18c515490edd3c5bfcdf208de0d106ad934
Bernie Bright <bbright@users.sourceforge.net>
2003-11-13 22:54:50 +0000

    Added command line arguments for initial paths.
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@174 65451dfc-fe10-0410-907f-9363c9a62196

--- src/main.cxx
+++ src/main.cxx
@@ -24,6 +24,8 @@
 #  include <config.h>
 #endif
 
+#include <string>
+#include <iostream>
 #include <FL/Fl.h>
 
 #if defined(WIN32)
@@ -32,9 +34,65 @@
 #  include "FGRun_Posix.h"
 #endif
 
+static std::string def_fg_exe = "";
+static std::string def_fg_root = "";
+static std::string def_fg_scenery = "";
+
+/**
+ * --fg-exe=<PATH>
+ * --fg-root=<DIR>
+ * --fg-scenery=<DIR>
+ */
+static int
+parse_arg( int, char** argv, int& i )
+{
+    if (strncmp( argv[i], "--fg-exe=", 9 ) == 0)
+    {
+	def_fg_exe.assign( &argv[i][9] );
+	++i;
+	return 1;
+    }
+
+    if (strncmp( argv[i], "--fg-root=", 10 ) == 0)
+    {
+	def_fg_root.assign( &argv[i][10] );
+	def_fg_scenery = def_fg_root;
+	def_fg_scenery += "/Scenery";
+
+	++i;
+	return 1;
+    }
+
+    if (strncmp( argv[i], "--fg-scenery=", 13 ) == 0)
+    {
+	def_fg_scenery.assign( &argv[i][13] );
+	++i;
+	return 1;
+    }
+
+//     if (strcmp( argv[i], "--advanced" ) == 0)
+//     {
+//     }
+//     if (strcmp( argv[i], "--wizard" ) == 0)
+//     {
+//     }
+
+    return 0;
+}
+
 int
 main( int argc, char* argv[] )
 {
+    int i = 0;
+    if (Fl::args( argc, argv, i, parse_arg ) < argc)
+    {
+	Fl::fatal("Options are:\n --fg-exe=<PATH>\n --fg-root=<DIR>\n --fg-scenery=<DIR>\n%s", Fl::help );
+    }
+
+//     std::cout << "def_fg_exe='" << def_fg_exe << "'\n";
+//     std::cout << "def_fg_root='" << def_fg_root << "'\n";
+//     std::cout << "def_fg_scenery='" << def_fg_scenery << "'\n";
+
     UserInterface* ui;
 
 #if defined(WIN32)
