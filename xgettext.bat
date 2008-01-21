586bbb16b505a66d5ca8d1482b06b4269ba0b588
Frederic Bouvier <fredfgfs01@free.fr>
2008-01-21 20:39:40 +0000

    Add the dutch translation from Stefan
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@420 65451dfc-fe10-0410-907f-9363c9a62196

--- xgettext.bat
+++ xgettext.bat
@@ -1,4 +1,5 @@
 ..\gettext\bin\xgettext -d fgrun -o po\fgrun.pot --copyright-holder="Frederic Bouvier" -k_ -kN_ -D src advanced.cxx wizard.cxx wizard_funcs.cxx advanced_funcs.cxx AirportBrowser.cxx AirportTable.cxx fgfsrc.cxx logwin.cxx io.cxx main.cxx
 cd po
 ..\..\gettext\bin\msgmerge -U fr.po fgrun.pot
+..\..\gettext\bin\msgmerge -U nl.po fgrun.pot
 cd ..
