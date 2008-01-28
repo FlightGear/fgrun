e80ccb698ed8ab8c11082bfb63eff4f864dcac29
Frederic Bouvier <fredfgfs01@free.fr>
2008-01-28 07:15:37 +0000

    Add german translation to build system
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@432 65451dfc-fe10-0410-907f-9363c9a62196

--- xgettext.bat
+++ xgettext.bat
@@ -1,6 +1,7 @@
-..\gettext\bin\xgettext -d fgrun -o po\fgrun.pot --copyright-holder="Frederic Bouvier" -k_ -kN_ -D src advanced.cxx wizard.cxx wizard_funcs.cxx advanced_funcs.cxx AirportBrowser.cxx AirportTable.cxx fgfsrc.cxx logwin.cxx io.cxx main.cxx
+..\gettext\bin\xgettext -d fgrun -o po\fgrun.pot --copyright-holder="Frederic Bouvier" -k_ -kN_ src/advanced.cxx src/wizard.cxx src/wizard_funcs.cxx src/advanced_funcs.cxx src/AirportBrowser.cxx src/AirportTable.cxx src/fgfsrc.cxx src/logwin.cxx src/io.cxx src/main.cxx
 cd po
 ..\..\gettext\bin\msgmerge -U fr.po fgrun.pot
 ..\..\gettext\bin\msgmerge -U nl.po fgrun.pot
 ..\..\gettext\bin\msgmerge -U pt.po fgrun.pot
+..\..\gettext\bin\msgmerge -U de.po fgrun.pot
 cd ..
