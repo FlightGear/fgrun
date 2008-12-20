f537fead85287125b582f32d5ef075ed10dfe334
Frederic Bouvier <fredfgfs01@free.fr>
2008-12-20 20:50:58 +0000

    Version 1.5.0
    git-svn-id: https://fgrun.svn.sourceforge.net/svnroot/fgrun/trunk/fgrun@485 65451dfc-fe10-0410-907f-9363c9a62196

--- src/advanced.cxx
+++ src/advanced.cxx
@@ -1745,6 +1745,7 @@ Advanced::Advanced() {
         o->labelsize(12);
         o->minimum(1);
         o->maximum(0);
+        o->step(0.01);
       }
       { Fl_Input* o = ceiling = new Fl_Input(235, 335, 80, 25, _("Ceiling:"));
         o->tooltip(_("FT_ASL[:THICKNESS_FT]"));
