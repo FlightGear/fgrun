b82da6b7d7701ebf156e00d09c7a057e31b724a1
Gijs de Rooy <gijsrooy@hotmail.com>
2013-07-25 19:52:42 +0200

    Fix Boost inclusion for Linux.

--- src/AirportTable.cxx
+++ src/AirportTable.cxx
@@ -24,7 +24,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <boost\algorithm\string\case_conv.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
 
 #include <FL/fl_draw.H>
 
